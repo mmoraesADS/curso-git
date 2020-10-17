498c7da5aee78693ec177d15c347a9e1e678d803
Matheus de Moraes e Silva <matheusmoraes.ads@gmail.com>
2020-10-17 19:25:28 -0300

    Projeto iniciado

---
+++ Course/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+            DateTime d1 = DateTime.ParseExact("2000-08-15", "yyyy-MM-dd", CultureInfo.InvariantCulture);
+            DateTime d2 = DateTime.ParseExact("15/08/2020 13:10:25", "dd/MM/yyyy HH:mm:ss", CultureInfo.InvariantCulture);
+            DateTime d3 = new DateTime(2001, 8, 15, 13, 45, 58, 275);
+            DateTime d4 = d3.AddHours(5.47);
+
+
+            TimeSpan t1 = TimeSpan.FromDays(1.5);
+
+
+            Console.WriteLine(t1);
+            Console.WriteLine(d3);
+            Console.WriteLine(d3.Date);
+            Console.WriteLine(d3.Day);
+            Console.WriteLine(d3.ToLongDateString());
+            Console.WriteLine(d3.ToLongTimeString());
+            Console.WriteLine(d3.ToShortDateString());
+            Console.WriteLine(d3.ToShortTimeString());
+            Console.WriteLine(d3.ToString());
+            Console.WriteLine(d3.ToString("dd/MM/yyyy"));
+            Console.WriteLine(d4);
+        }
+
+    }
+}
---
+++ Course/funcionario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Course {
+    class funcionario {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public double Salario { get; private set; }
+
+        public funcionario(int id, string nome, double salario) {
+            Id = id;
+            Nome = nome;
+            Salario = salario;
+        }
+
+        public void Aumento(double aumento) {
+            Salario *= (aumento + 1);
+
+        }
+        override
+        public string ToString() => $"\nID: {Id} \nNome: {Nome} \nSalário: {Salario}";
+
+    }
+}
